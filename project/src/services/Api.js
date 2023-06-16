ccf417acd1780035f0f6a69d61891b138b224965
Matheus Zauza Maschietto <matheus_maschietto@yahoo.com.br>
2023-06-15 22:11:26 -0300

    Update Api.js

--- project/src/services/Api.js
+++ project/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create(
     {
-        baseURL: 'http://10.220.219.61:8080',
+        baseURL: 'http://10.220.218.254:8080',
         headers: {
             "Content-Type": "application/json"
         }
